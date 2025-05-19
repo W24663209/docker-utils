599f6580adfc05a7631477e359c01d90c582c6c6
week <w24663209@gmail.com>
2025-05-19 12:47:07 +0000

    1

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def get_container_stats(container):
     cpu_stats = stats['cpu_stats']
 
     memory_usage = memory_stats['usage'] / (1024 * 1024 * 1024)  # 内存使用量（GB）
-    memory_usage_formatted = "{:.2f}G".format(memory_usage)  # 保留两位小数
+    memory_usage_formatted = "{:.2f}".format(memory_usage)  # 保留两位小数
     cpu_usage = cpu_stats['cpu_usage']['total_usage']
     system_cpu_usage = cpu_stats['system_cpu_usage']
     online_cpus = cpu_stats['online_cpus']
 
     cpu_percent = (cpu_usage / (system_cpu_usage / online_cpus)) * 100
-    cpu_percent_ = "{:.2f}%".format(cpu_percent)  # 保留两位小数
+    cpu_percent_ = "{:.2f}".format(cpu_percent)  # 保留两位小数
     return container.name, memory_usage_formatted, cpu_percent_
 
 def get_container_memory_and_cpu_usage():
@@ -38,8 +38,9 @@ def get_container_memory_and_cpu_usage():
                 })
             except Exception as e:
                 print(f"获取失败 {container.name}: {e}")
-
-    return stats_data
+    # 按内存使用量从大到小排序
+    stats_data_sorted = sorted(stats_data, key=lambda x: x['memory_usage'], reverse=True)
+    return stats_data_sorted
 
 @app.route('/containers/stats', methods=['GET'])
 def get_stats():
